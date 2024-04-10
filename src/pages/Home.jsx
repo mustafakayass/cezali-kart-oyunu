9b3efe0128e42f0bb2905f9a34ff7590fd4cc4bf
mustafakayass <mustafakayas@icloud.com>
2024-04-10 23:17:36 +0300

    analytics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Card from "../components/Card";
 import Layout from "../components/Layout";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function Home() {
     return (
       <Layout>
         <Card/>
+        <Analytics/>
       </Layout>
     )
   }
