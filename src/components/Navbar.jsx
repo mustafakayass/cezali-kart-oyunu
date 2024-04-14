90d53cdc4cbc225d652e5a37969ad52f39c79c24
mustafakayass <mustafakayas@icloud.com>
2024-04-14 16:46:57 +0300

    refresh via navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  // dropdown menünün açık/kapalı durumunu tutan state
-  const [isOpen, setIsOpen] = useState(false);
 
-  // dropdown menüyü açmak veya kapatmak için kullanılan fonksiyon
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const handleRefresh = () => {
+    window.location.reload(); // Sayfayı yenilemek için window.location.reload() kullanılıyor
   };
+
     return (
       <div className="bg-base-200 py-2">
         <div className="max-w-7xl mx-auto navbar">
           <div className="navbar-start">
-            <Link href="/" className="btn btn-ghost normal-case text-2xl">
+            <Link href="/" className="btn btn-ghost normal-case text-2xl" onClick={handleRefresh}>
               Cezalı Kart Oyunu
             </Link>
           </div>
